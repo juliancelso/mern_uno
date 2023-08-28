9b1eaa5c3974915edacba23504781bef590d7148
juliancelso <juliancelsoginzburg@gmail.com>
2023-08-28 17:36:45 -0300

    Nineth Commit

--- saludo-exp-hbs/routes/index.js
+++ saludo-exp-hbs/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pagina inicial' });
 });
-
+router.post('/', function(req, res, next){
+  res.render('saludar', { title: 'Saludo', nombre: req.body.nom});
+});
 module.exports = router;
